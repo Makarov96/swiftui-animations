e0b743a23d60b16fd1b636d37d85e654d56af9f3
Makarov96 <estev97@gmail.com>
2024-05-01 21:40:34 -0600

    Initial Commit

---
+++ animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  animations
+//
+//  Created by Guerin Steven Colocho Chacon on 1/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Guerin Steven Colocho Chacon on 1/05/24.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
